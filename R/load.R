628b4ed112decb5285d8d6db43bb107c7861e8a9
Pedro J. Molina <pjmolina@gmail.com>
2018-03-17 12:40:53 +0100

    document

--- R/load.R
+++ R/load.R
@@ -19,8 +19,23 @@ numericCols <- sapply(test, is.numeric)
 categoricalCols <- sapply(test, is.character)
 
 colnames(test[numericCols])
+
+# [1] "Id"            "MSSubClass"    "LotFrontage"   "LotArea"       "OverallQual"   "OverallCond"  
+# [7] "YearBuilt"     "YearRemodAdd"  "MasVnrArea"    "BsmtFinSF1"    "BsmtFinSF2"    "BsmtUnfSF"    
+# [13] "TotalBsmtSF"   "1stFlrSF"      "2ndFlrSF"      "LowQualFinSF"  "GrLivArea"     "BsmtFullBath" 
+# [19] "BsmtHalfBath"  "FullBath"      "HalfBath"      "BedroomAbvGr"  "KitchenAbvGr"  "TotRmsAbvGrd" 
+# [25] "Fireplaces"    "GarageYrBlt"   "GarageCars"    "GarageArea"    "WoodDeckSF"    "OpenPorchSF"  
+# [31] "EnclosedPorch" "3SsnPorch"     "ScreenPorch"   "PoolArea"      "MiscVal"       "MoSold"       
+# [37] "YrSold"    
+
 colnames(test[categoricalCols])
 
-
-
+# [1] "MSZoning"      "Street"        "Alley"         "LotShape"      "LandContour"   "Utilities"    
+# [7] "LotConfig"     "LandSlope"     "Neighborhood"  "Condition1"    "Condition2"    "BldgType"     
+# [13] "HouseStyle"    "RoofStyle"     "RoofMatl"      "Exterior1st"   "Exterior2nd"   "MasVnrType"   
+# [19] "ExterQual"     "ExterCond"     "Foundation"    "BsmtQual"      "BsmtCond"      "BsmtExposure" 
+# [25] "BsmtFinType1"  "BsmtFinType2"  "Heating"       "HeatingQC"     "CentralAir"    "Electrical"   
+# [31] "KitchenQual"   "Functional"    "FireplaceQu"   "GarageType"    "GarageFinish"  "GarageQual"   
+# [37] "GarageCond"    "PavedDrive"    "PoolQC"        "Fence"         "MiscFeature"   "SaleType"     
+# [43] "SaleCondition"
 
